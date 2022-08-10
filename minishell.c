614a3b6c027a74815837f405a20960b9f6e943c3
Scabbers Tapioca <stapioca@il-j6.msk.21-school.ru>
2022-08-10 21:09:37 +0300

    3

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: stapioca <stapioca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 19:06:00 by stapioca          #+#    #+#             */
-/*   Updated: 2022/08/10 20:48:19 by stapioca         ###   ########.fr       */
+/*   Updated: 2022/08/10 20:58:57 by stapioca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,8 @@ int	main(int argc, char **argv, char **env)
 	(void)argv;
 	(void)env;
 	//g_sh.str = strdup("co$USER mma'n\\nn\'dd000\\\'00co\"mm\\\"\"an\'dddd\'a");
-	g_sh.str = strdup(" 1 2 3| 				4    55555 6|7 \"8\"8\"8 \" 9");
+	g_sh.str = strdup("\"8\"8\"8\"");
+	//g_sh.str = strdup(" 1 2 3| 				4    55555 6|7 \"8\"8\"8 \" 9");
 	//g_sh.str = ft_strdup("\"a\"	b\"	c\"	d\"\"\" 	 \'	   \'  b");
 	//g_sh.str = ft_strdup("\"a\"	b\"	c\"	d\"\"\" 	 \'	   \'  b");
 	init_shell();
