80870337bfdaef5b7bc67654d52615194c3b4d0e
njohanne <aleksey88.avt@gmail.com>
2022-08-15 15:56:28 +0300

    readline

--- minishell.c
+++ minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: stapioca <stapioca@student.42.fr>          +#+  +:+       +#+        */
+/*   By: njohanne <njohanne@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 19:06:00 by stapioca          #+#    #+#             */
-/*   Updated: 2022/08/11 21:24:34 by stapioca         ###   ########.fr       */
+/*   Updated: 2022/08/15 15:11:12 by njohanne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,16 +28,20 @@ int	main(int argc, char **argv, char **env)
 
 	(void)argc;
 	(void)argv;
+	//(void)env;
 	//g_sh.str = strdup("co$USER mman\\nn\'dd000\\\'00co\"mm\\\"\"an\"\'dddd\'a");
 	//g_sh.str = strdup("\"8\"8\"8\"");
 	//g_sh.str = strdup(" 1 2 3| 				4    55555 6|7 \"8\"8\"8 \" 9");
-	g_sh.str = strdup(" 1 2 3| 				4    55555 6|7 \'\"8\'\"6\"8\'8\'8\"9\"      dfgghfg      ");
+	//g_sh.str = strdup(" 1 2 3| 				4    55555 6|7 \'\"8\'\"6\"8\'8\'8\"9\"      dfgghfg      ");
 	//g_sh.str = ft_strdup("\"a\"	b\"	c\"	d\"\"\" 	 \'	   \'  b");
 	//g_sh.str = ft_strdup("\"a\"	b\"	c\"	d\"\"\" 	 \'	   \'  b");
+	g_sh.str = ft_strdup ("text");
 	init_shell();
 	while (!g_sh.stop_flag)
 	{
+		g_sh.str = readline("minishell $");
 		ch_err = lexer(g_sh.str);
+		ch_err = 0;
 		if (!ch_err)
 		{
 			printf("g_sh.str= %s\n", g_sh.str);
@@ -47,5 +51,6 @@ int	main(int argc, char **argv, char **env)
 		else
 			printf("command error\n");
 	}
+	free(g_sh.str);
 	return (0);
 }
