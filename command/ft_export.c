603b236379eaaf4c152c008abded98e382e4d9b7
njohanne <aleksey88.avt@gmail.com>
2022-09-30 17:17:16 +0300

    продолжение export

--- command/ft_export.c
+++ command/ft_export.c
@@ -6,7 +6,7 @@
 /*   By: njohanne <njohanne@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/09 15:55:17 by njohanne          #+#    #+#             */
-/*   Updated: 2022/09/29 20:16:56 by njohanne         ###   ########.fr       */
+/*   Updated: 2022/09/30 17:12:40 by njohanne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,57 +35,61 @@ int	ft_search_nvar(char **env, char *str)
 			if((env[i][j + 1] == '=' || env[i][j + 1] == '\0') && j == len)
 			{
 				free(var);
-				return (0);
+				return (i);
 			}
 			j++;
 		}
 	}	
 	free(var);
-	return(1);
+	return(0);
 }
 
-int	ft_tyt_nax(char *env, char *arv)
+char	**ft_nenv(char **env, char *str, int i)
 {
-	int	i;
+	int		j;
+	int		len;
+	char	**nenv;
 
-	i = 0;
-	while (env[i] == arv[i])
+	j = -1;
+	len = ft_len_env(env);
+	nenv = (char **)malloc(sizeof(char **) * (len + 1));
+	nenv[len] = NULL;
+	while (env[++j])
 	{
-		if ((env[i + 1] == '=' || env[i + 1] == '\0') && (arv[i + 1] == '\0' || arv[i + 1] == '='))
-			return(1);
-		i++;
+		if (j != i)
+		{
+			nenv[j] = (char *)malloc(sizeof(char *) * ft_strlen(env[j]));
+			nenv[j] = ft_memcpy(nenv[j], env[j], ft_strlen(env[j]));
+		}
+		else
+		{
+			nenv[j] = (char *)malloc(sizeof(char *) * ft_strlen(str));
+			nenv[j] = ft_memcpy(nenv[j], str, ft_strlen(str));
+		}
 	}
-	return(0);
+	free(str);
+	return(nenv);
 }
 
 char	**ft_env_join(char **env, char **arv)
 {
 	char	**nenv;
+	char	*str;
 	int		i;
 	int		len;
 
-	i = -1;
-	if (!ft_search_nvar(env, arv[1]))
+	i = ft_search_nvar(env, arv[1]);
+	if (i > 0)
 	{
-		len = ft_len_env(env);
-		nenv = (char **)malloc(sizeof(char**) * (len + 1));
-		nenv[len] = NULL;
-		while (env[++i])
-		{
-			if (!ft_tyt_nax(env[i], arv[1]))
-			{
-				nenv[i] = (char *)malloc(sizeof(char *) * ft_strlen(env[i]));
-				nenv[i] = ft_memcpy(nenv[i], env[i], ft_strlen(env[i]));
-			}
-			else
-			{
-				nenv[i] = (char *)malloc(sizeof(char *) * ft_strlen(arv[1]));
-				nenv[i] = ft_memcpy(nenv[i], arv[1], ft_strlen(arv[1]));
-			}
-		}
+		len = ft_strlen(arv[1]);
+		str = (char *)malloc(sizeof(char *) * (len + 1));
+		str[len] = '\0';
+		str = ft_memcpy(str, arv[1], len);
+		nenv = ft_nenv(env, str, i);
 	}
 	else
 	{
+		i = -1;
 		len = ft_len_env(env) + 1;
 		nenv = (char **)malloc(sizeof(char **) * (len + 1));
 		nenv[len] = NULL;
